ab3775e93a2f22b2788444decef6ab261ee991a3
kennyvel <kennyvel805@gmail.com>
2025-03-13 16:31:42 -0700

    General Styling and Die Component
    Styling the main containers and the die for the page so everything is centered. Implemented a basic die component with a passed in value and a button.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Die } from './components/Die'
 
 function App() {
   return (
-    <main></main>
+    <main>
+      <div className="die-container">
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+        <Die value={5}/>
+        <Die value={6}/>
+        <Die value={7}/>
+        <Die value={8}/>
+        <Die value={9}/>
+        <Die value={10}/>
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Die.jsx
@@ -0,0 +1,11 @@
+export function Die(props) {
+    const handleClick = () => {
+        // Will eventually turn the button green and select the number to be kept
+        alert('Button clicked!');
+    };
+    return (
+        <div className="die">
+            <button onClick={handleClick}>{props.value}</button>
+        </div>
+    )
+}
