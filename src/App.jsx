f9e72ae148ae7105c6b2e40781933fc887c2684a
kennyvel <kennyvel805@gmail.com>
2025-03-15 17:34:52 -0700

    Stylistic changes
    Changing some styles for the die. Also putting all the functions in App.jsx inside of the App function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Die } from './components/Die'
 
-function generateAllNewDice() {
-  const diceArray = [];
-  for (let i = 0; i < 10; i++) {
-    diceArray.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return diceArray;
+export default function App() {
+	const [dice, setDice] = useState(generateAllNewDice())
+
+	function generateAllNewDice() {
+		const diceArray = [];
+		for (let i = 0; i < 10; i++) {
+			diceArray.push(Math.ceil(Math.random() * 6))
+		}
+		return diceArray;
+	}
+	const diceElements = dice.map(value => <Die value={value} />)
+
+	return (
+		<main>
+			<div className="die-container">
+			 {diceElements}
+			</div>
+		</main>
+	)
 }
-
-function App() {
-  const diceArray = allNewDice();
-
-  const die = diceArray.map((value) => (
-    <Die value={value} />
-  ))
-  
-  return (
-    <main>
-      <div className="die-container">
-        {die}
-      </div>
-    </main>
-  )
-}
-
-export default App
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,12 +1,12 @@
 export function Die(props) {
-    const handleClick = () => {
-        // Will eventually turn the button green and select the number to be kept
-        alert('Button clicked!');
-    };
-    return (
-        <div className="die">
-            <button onClick={handleClick}>{props.value}</button>
-        </div>
-    )
+	const handleClick = () => {
+		// Will eventually turn the button green and select the number to be kept
+		alert('Button clicked!')
+	}
+	return (
+		<div className="die">
+			<button onClick={handleClick}>{props.value}</button>
+		</div>
+	)
 }
 
