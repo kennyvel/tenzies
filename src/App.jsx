98da35da13badaccf47ab0f5a8393230556623f9
kennyvel <kennyvel805@gmail.com>
2025-03-19 20:33:00 -0700

    Win Confetti
    Adding confetti that falls on the screen when the player wins.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { Die } from './components/Die'
 import { nanoid } from "nanoid"
+import Confetti from 'react-confetti'
 
 export default function App() {
 	const [dice, setDice] = useState(generateAllNewDice())
-
+	
 	// Can't use .map() since it will return a new array and not a boolean
 	const gameWon = (dice.every(die => die.isHeld) && 
 					 dice.every(die=>die.value === dice[0].value))
@@ -53,6 +54,7 @@ export default function App() {
 
 	return (
 		<main>
+			{gameWon && <Confetti />}
 			<h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
 			<div className="die-container">
